70dc8764eeda45a1347808df0407576ce0725a53
AIFC2025 <yingtongzhou123@gmail.com>
2025-09-11 01:24:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => ({
     outDir: "dist/spa",
   },
   plugins: [react(), expressPlugin()],
-  base: '/AuscultSim/',
+  base: "/AuscultSim",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client"),
